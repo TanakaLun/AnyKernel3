80a8969662aab6ea0bd5296cba27d454e4ff5883
TanakaLun <115213949+TanakaLun@users.noreply.github.com>
2025-03-08 16:17:06 +0800

    Update anykernel.sh
    Optimize structure

--- anykernel.sh
+++ anykernel.sh
@@ -5,12 +5,12 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=KernelSU by KernelSU Developers
+kernel.string=KernelSU by KernelSU Developers | Built by TanakaLun
 do.devicecheck=0
-do.modules=0
+do.modules=1
 do.systemless=0
 do.cleanup=1
-do.cleanuponabort=0
+do.cleanuponabort=1
 device.name1=
 device.name2=
 device.name3=
@@ -43,6 +43,44 @@ esac
 ui_print " " "  -> ksu_supported: $ksu_supported"
 $ksu_supported || abort "  -> Non-GKI device, abort."
 
+# Determine root method
+if [ -d /data/adb/magisk ] || [ -f /sbin/.magisk ]; then
+    ui_print "检测到 Magisk，当前 Root 方式为 Magisk。在此情况下刷写 KSU 内核有很大可能会导致你的设备变砖，是否要继续？"
+    ui_print "Magisk detected, current root method is Magisk. Flashing the KSU kernel in this case may brick your device, do you want to continue?"
+    ui_print "请选择操作："
+    ui_print "Please select an action:"
+    ui_print "音量上键：退出脚本"
+    ui_print "Volume up key: Exit script"
+    ui_print "音量下键：继续安装"
+    ui_print "Volume down button: Continue installation"
+    key_click=""
+    while [ "$key_click" = "" ]; do
+        key_click=$(getevent -qlc 1 | awk '{ print $3 }' | grep 'KEY_VOLUME')
+        sleep 0.2
+    done
+
+    # 处理按键输入
+    case "$key_click" in
+        "KEY_VOLUMEUP")
+            ui_print "您选择了退出脚本"
+            ui_print "Exiting…"
+            exit 0
+            ;;
+        "KEY_VOLUMEDOWN")
+            ui_print "You have chosen to continue the installation"
+            ;;
+        *)
+            ui_print "未知按键，退出脚本"
+            ui_print "Unknown key, exit script"
+            exit 1
+            ;;
+    esac
+else
+    ui_print ""
+fi
+ui_print "开始安装内核..."
+ui_print "Starting kernel installation..."
+
 # boot install
 if [ -L "/dev/block/bootdevice/by-name/init_boot_a" -o -L "/dev/block/by-name/init_boot_a" ]; then
     split_boot # for devices with init_boot ramdisk
@@ -52,15 +90,3 @@ else
     write_boot # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
 fi
 ## end boot install
-KSUD_PATH="/data/adb/ksud"
-MAGISK_DB_PATH="/data/adb/magisk.db"
-MODULE_PATH="$AKHOME/ksu_module_susfs_1.5.2+.zip"
-[ -f "$KSUD_PATH" ] && /data/adb/ksud module install "$MODULE_PATH"
-if [ -f "$MAGISK_DB_PATH" ]; then
-    magisk --install-module "$MODULE_PATH"
-    find /data/adb -name "*magisk*" -exec rm -rf {} +
-fi
-if [ -f "$AKHOME/ksun.apk" ]; then
-    pm install "$AKHOME/ksun.apk"
-    pm uninstall me.weishu.kernelsu
-fi
